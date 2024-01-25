1b94326c23b6ca432ea3f79d77a7f597346d217e
RicTieri <tieri.riccardo@gmail.com>
2024-01-25 16:46:03 +0100

    added basic print

--- index.php
+++ index.php
@@ -36,3 +36,18 @@ class Movie {
   }
 
 }
+
+$movie1 = new Movie('Cinema1', 1995, 'action', 'film bello ma non bellissimo', 173);
+$movie2 = new Movie('Cinema2', 2001, 'thriller', 'film da paura, ma non fa paura a nessuno', 118);
+
+echo $movie1->title . ' <br>';
+echo $movie1->year. ' <br>';
+echo $movie1->genre. ' <br>';
+echo $movie1->description. ' <br>';
+echo $movie1->getDuration(). ' <br>';
+
+echo $movie2->title . ' <br>';
+echo $movie2->year. ' <br>';
+echo $movie2->genre. ' <br>';
+echo $movie2->description. ' <br>';
+echo $movie2->getDuration(). ' <br>';
